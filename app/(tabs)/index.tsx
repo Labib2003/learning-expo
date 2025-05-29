d70a453690f0880da5fb3b79c633d5f87d88db72
Labib2003 <labib.salimi30@gmail.com>
2025-05-29 11:53:49 +0600

    feat: add and style the home screen
    Added home screen and styled it with tailwind.
    Used the image component from expo image to render an image.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,38 @@
-import { Text, View } from 'react-native';
+import { ImageIcon } from 'lucide-react-native';
+import { Pressable, Text, View } from 'react-native';
+import { Image } from 'expo-image';
+
+// @ts-expect-error: No types for this image
+import placeholderImage from '../../assets/images/background-image.png';
 
 export default function HomeScreen() {
   return (
-    <View className="flex h-full items-center justify-center bg-slate-700">
-      <Text className="text-3xl font-bold text-white">Home</Text>
+    <View className="flex h-full items-center justify-center gap-3 bg-slate-700 p-5">
+      <View className="w-full flex-1 overflow-hidden">
+        <Image
+          source={placeholderImage}
+          style={{
+            width: '100%',
+            height: '100%',
+            resizeMode: 'contain',
+          }}
+        />
+      </View>
+
+      <View className="w-full gap-2">
+        <Pressable
+          className="flex flex-row items-center justify-center gap-3 rounded-lg bg-white p-3"
+          onPress={() => alert('Photo selected!')}>
+          <ImageIcon />
+          <Text className="text-center font-semibold">Choose A Photo</Text>
+        </Pressable>
+
+        <Pressable
+          className="flex flex-row items-center justify-center gap-3 rounded-lg bg-slate-800 p-3"
+          onPress={() => alert('Photo selected!')}>
+          <Text className="text-center font-semibold text-white">Use This Photo</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
